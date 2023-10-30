ceb64d3c3beafc14db083b0c55ee4a4d6320e3bd
Bethuel Dela Cruz <jbethueldc@gmail.com>
2023-10-30 17:03:54 +0800

    Initial commit

---
+++ src/data/siteMetadata.ts
@@ -0,0 +1,17 @@
+export const siteMetadata = {
+  theme: "system",
+  siteUrl: "https://tailwind-nextjs-starter-blog.vercel.app",
+  title: "Next.js Starter Blog",
+  description: "A blog created with Next.js and Tailwind.css",
+  socialBanner: "/static/images/twitter-card.png",
+  language: "en-us",
+  headerTitle: "TailwindBlog",
+  email: "jbethueldc@gmail.com",
+  author: "jbethueldc@gmail.com",
+
+  github: "https://github.com",
+  twitter: "https://twitter.com/Twitter",
+  facebook: "https://facebook.com",
+  youtube: "https://youtube.com",
+  linkedin: "https://www.linkedin.com",
+};
