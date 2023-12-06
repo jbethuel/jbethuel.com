c132ca4d2c32763784912e20803a83538f4f24a4
Bethuel Dela Cruz <jbethueldc@gmail.com>
2023-12-06 10:26:47 +0800

    cleanup

--- pages/gallery/index.tsx
+++ pages/gallery/index.tsx
@@ -5,14 +5,40 @@ import { NextSeo } from 'next-seo';
 import { Fragment, useCallback, useEffect, useState } from 'react';
 import { createPortal } from 'react-dom';
 
-const imageUrls = [
-  'https://s3.ap-southeast-1.amazonaws.com/w-assets.jbethuel.com/IMG_8817.jpeg',
-  'https://s3.ap-southeast-1.amazonaws.com/w-assets.jbethuel.com/IMG_8824.jpeg',
-  'https://s3.ap-southeast-1.amazonaws.com/w-assets.jbethuel.com/IMG_9720.jpeg',
-  'https://s3.ap-southeast-1.amazonaws.com/w-assets.jbethuel.com/IMG_8864.jpeg',
-  'https://s3.ap-southeast-1.amazonaws.com/w-assets.jbethuel.com/IMG_8948.jpeg',
-  'https://s3.ap-southeast-1.amazonaws.com/w-assets.jbethuel.com/IMG_9738.jpeg',
-  'https://s3.ap-southeast-1.amazonaws.com/w-assets.jbethuel.com/IMG_9439.jpeg',
+type Image = {
+  url: string;
+  alt: string;
+};
+
+const imageUrls: Image[] = [
+  {
+    url: 'https://assets.jbethuel.com/IMG_8817.jpeg',
+    alt: 'wtf',
+  },
+  {
+    url: 'https://assets.jbethuel.com/IMG_8824.jpeg',
+    alt: 'falls',
+  },
+  {
+    url: 'https://assets.jbethuel.com/IMG_9720.jpeg',
+    alt: 'mountains',
+  },
+  {
+    url: 'https://assets.jbethuel.com/IMG_8864.jpeg',
+    alt: 'oslob',
+  },
+  {
+    url: 'https://assets.jbethuel.com/IMG_8948.jpeg',
+    alt: 'ruins',
+  },
+  {
+    url: 'https://assets.jbethuel.com/IMG_9738.jpeg',
+    alt: 'old player',
+  },
+  {
+    url: 'https://assets.jbethuel.com/IMG_9439.jpeg',
+    alt: 'foggy morning',
+  },
 ];
 
 const ClientPortal = ({ children, selector }: { children: React.ReactNode; selector: string }) => {
@@ -39,40 +65,19 @@ export default function Gallery() {
       <ClientPortal selector={portalId}>
         <section>
           <div className="columns-1 gap-5 sm:columns-2 sm:gap-8 md:columns-3 lg:columns-4 [&>img:not(:first-child)]:mt-8">
-            {imageUrls.map((image, key) => {
+            {imageUrls.map(({ url, alt }, index) => {
               return (
                 // eslint-disable-next-line @next/next/no-img-element
                 <img
                   className="hover-images cursor-pointer"
                   loading="lazy"
-                  key={key}
-                  src={image}
-                  alt={image}
-                  onClick={() => onClickImage(image)}
+                  key={index}
+                  src={url}
+                  alt={alt}
+                  onClick={() => onClickImage(url)}
                 />
               );
             })}
-          </div>
-
-          <div
-            className="grid sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4"
-            style={{ display: 'none' }}
-          >
-            {imageUrls.map((image, key) => (
-              <div
-                key={key}
-                className="hover-images cursor-pointer"
-                onClick={() => onClickImage(image)}
-              >
-                {/* eslint-disable-next-line @next/next/no-img-element */}
-                <img
-                  className="h-auto max-w-full rounded-sm"
-                  loading="lazy"
-                  src={image}
-                  alt={image}
-                />
-              </div>
-            ))}
           </div>
         </section>
       </ClientPortal>
