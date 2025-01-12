4f594eb5207e67619c0e3ccc3cbd28d6251fbc8b
Bethuel Dela Cruz <jbethueldc@gmail.com>
2025-01-13 05:36:05 +0800

    fix build

---
+++ src/components/header.tsx
@@ -0,0 +1,25 @@
+import { CustomLink } from "./custom-link"
+import { HeaderItem } from "./header-item"
+import { ThemeSwitch } from "./theme-switch"
+
+export function Header() {
+  return (
+    <header className="flex flex-row justify-between">
+      <h1 className="font-bold">
+        <CustomLink href="/">JBethuel</CustomLink>
+      </h1>
+      <nav className="flex flex-row justify-between space-x-4 place-items-center">
+        <CustomLink className="font-medium" href="/">
+          <HeaderItem label="Home" route="/" />
+        </CustomLink>
+        <CustomLink className="font-medium" href="/links">
+          <HeaderItem label="Links" route="/links" />
+        </CustomLink>
+        <CustomLink className="font-medium" href="/gallery">
+          <HeaderItem label="Gallery" route="/gallery" />
+        </CustomLink>
+        <ThemeSwitch />
+      </nav>
+    </header>
+  )
+}
